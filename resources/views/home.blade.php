401498a17ae7ff4c6170014f65269d0a687bbbce
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-02-01 17:24:30 -0600

    [skip] remove scrollbars & outdated css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,7 @@
 @extends('app')
 
 @section('content')
-    <div class="ui one column centered grid" style="padding-top: 20px;">
+    <div class="ui one column" style="padding-top: 20px;">
         <img class="ui large centered image" src="{{ asset('images/panda-logo.png') }}" />
     </div>
-@endsection
-
-@section('inline-css')
-    <script type="text/css">
-        .big-image {
-            width: 100%;
-            min-width: 484px;
-            min-height: 720px;
-        }
-    </script>
 @endsection
