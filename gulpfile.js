4133850b79b5f8889dc4071fd2e3f0faaf28e2d5
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-02-01 15:34:18 -0600

    Asset Cleanup
     - remove bootstrap
     - add elixir
     - add semantic/jquery via npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var elixir = require('laravel-elixir');
 
+var paths = {
+ 'semantic': './node_modules/semantic-ui/dist/',
+ 'jquery': './node_modules/jquery/dist/'
+};
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,5 +17,17 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+ mix
+     .copy(paths.semantic + 'themes/default', 'public/default')
+     .copy(paths.jquery + 'jquery.js', 'public/js/jquery.js')
+     .styles([
+         paths.semantic + 'semantic.css'
+     ], "public/css/app.css", "./")
+     .scripts([
+         paths.semantic + 'semantic.js'
+     ], "public/js/app.js", "./")
+     .version([
+         'js/*',
+         'css/*'
+     ], "public");
 });
