c062ec43996691e9106a9cbc0f6fad147e2c61b1
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-02-28 12:57:01 -0600

    [skip] allow manual recaching of GTs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::get('/profile/check-for-update/{gamertag}', 'ProfileController@checkForUpdate');
-Route::get('/profile/{gamertag}/{characterId?}', 'ProfileController@index');
 Route::get('/profile/manual-update/{seo}', 'ProfileController@manualUpdate');
+Route::get('/profile/{gamertag}/{characterId?}', 'ProfileController@index');
 
 Route::controller('/games', 'GameController');
 Route::controller('/admin', 'AdminController');
