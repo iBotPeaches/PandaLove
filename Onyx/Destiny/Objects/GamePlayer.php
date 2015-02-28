e8099cc4d5e803af568a8bb2ae7532588fc4f75b
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-02-28 09:15:19 -0600

    [skip] added note

--- Onyx/Destiny/Objects/GamePlayer.php
+++ Onyx/Destiny/Objects/GamePlayer.php
@@ -93,6 +93,8 @@ class GamePlayer extends Model {
 
     public function historyAccount()
     {
+        // We us this function to only return clanName when counting "PandaLove" members
+        // in a game. This prevents a nasty SELECT * FROM accounts, etc
         return $this->hasOne('Onyx\Account', 'membershipId', 'membershipId')
             ->select('id', 'membershipId', 'clanName');
     }
