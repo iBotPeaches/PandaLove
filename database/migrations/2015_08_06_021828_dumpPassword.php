22f95ad3953a2ab2ece1d131846013fbdaf15f55
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-08-05 21:20:00 -0500

    remove password column

---
+++ database/migrations/2015_08_06_021828_dumpPassword.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DumpPassword extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->string('password', 60);
+        });
+    }
+}
