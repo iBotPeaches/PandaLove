81b0ad49eb5cde42ec69ca3940ca943a20a2288b
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-08-12 06:48:31 -0500

    Tweak for redis socket

--- config/database.php
+++ config/database.php
@@ -115,11 +115,11 @@ return [
 		'cluster' => false,
 
 		'default' => [
+			'scheme' => env('REDIS_SCHEME', 'tcp'),
 			'host'     => '127.0.0.1',
 			'port'     => 6379,
 			'database' => 0,
+			'path' => '/tmp/redis.sock',
 		],
-
 	],
-
 ];
