6ea52f8fb484314d697b798dba569182ff30c79a
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-09-09 15:12:52 -0500

    move home/sign-in/signout to icons

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,14 +1,16 @@
 <nav id="nav">
     <ul class="container">
-        <li><a class="{{ HTML::activeClass('/') }}" href="{{ URL::to('/') }}">Home</a></li>
+        <li><a class="{{ HTML::activeClass('/') }}" href="{{ URL::to('/') }}"><i class="home icon"></i></a></li>
         <li><a class="{{ HTML::activeClass('profile') | HTML::activeClass('roster') }}" href="{{ URL::action('RosterController@getIndex') }}">Roster</a></li>
         <li><a class="{{ HTML::activeClass('games') }}" href="{{ URL::action('GameController@getIndex') }}">Games</a></li>
-        @if (isset($user) && $user != null && $user->account->isPandaLove())
-            <li><a class="{{ HTML::activeClass('calendar') }}" href="{{ URL::action('CalendarController@getIndex') }}">Calendar</a></li>
-            <li><a class="{{ HTML::activeClass('usercp') }}" href="{{ URL::action('UserCpController@getIndex') }}">Control Panel</a></li>
-            <li><a href="{{ URL::action('UserCpController@getLogout') }}">Logout</a></li>
+        @if (isset($user) && $user != null)
+            @if ($user->account->isPandaLove())
+                <li><a class="{{ HTML::activeClass('calendar') }}" href="{{ URL::action('CalendarController@getIndex') }}">Calendar</a></li>
+            @endif
+            <li><a class="{{ HTML::activeClass('usercp') }}" href="{{ URL::action('UserCpController@getIndex') }}">Options</a></li>
+            <li><a href="{{ URL::action('UserCpController@getLogout') }}"><i class="sign out icon"></i></a></li>
         @else
-            <li><a href="{{ URL::action('AuthController@getLogin') }}">Sign In</a></li>
+            <li><a href="{{ URL::action('AuthController@getLogin') }}"><i class="google plus icon"></i></a></li>
         @endif
     </ul>
 </nav>
