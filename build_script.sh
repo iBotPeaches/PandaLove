ef1d25eb65141e1d32c75e1f9974607fc85cfeef
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-09-22 09:48:35 -0500

    add shebang to build script

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 composer install;
 mysql --user=connor_testing --password=gmsZbtvEhEM9 -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=gmsZbtvEhEM9 connor_testing;
 ./artisan migrate --env=testing;
