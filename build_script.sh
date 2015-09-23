d77763727da369b4650983c3842423fa626c7388
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-09-23 14:41:46 +0000

    add gulp instruction

--- build_script.sh
+++ build_script.sh
@@ -3,4 +3,5 @@ composer install;
 mysql --user=connor_testing --password=gmsZbtvEhEM9 -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=gmsZbtvEhEM9 connor_testing;
 ./artisan migrate --env=testing;
 ./artisan db:seed --env=testing;
+gulp
 phpunit;
