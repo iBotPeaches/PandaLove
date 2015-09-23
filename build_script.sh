c92dc3a62b39842c6e4168a82f61781788faa2f6
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-09-23 22:40:40 +0000

    dont murder NPM servers

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 composer install;
-npm install;
+npm install --cache-min 9999999;
 npm install gulp;
 mysql --user=connor_testing --password=gmsZbtvEhEM9 -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=gmsZbtvEhEM9 connor_testing;
 ./artisan migrate --env=testing;
