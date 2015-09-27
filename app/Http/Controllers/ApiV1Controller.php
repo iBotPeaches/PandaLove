9e7d66a99d16c302dd2752e6d5479ec593484afd
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-09-27 14:27:35 -0500

    remove double quotes from date

--- app/Http/Controllers/ApiV1Controller.php
+++ app/Http/Controllers/ApiV1Controller.php
@@ -306,7 +306,8 @@ class ApiV1Controller extends Controller {
                     ->where('admin', true)
                     ->firstOrFail();
 
-                $all['start'] = new Carbon($all['start']);
+                // hack - mutators won't work since start is date
+                $all['start'] = new Carbon(str_replace('"', null, $all['start']));
 
                 $gameEvent = new GameEvent();
                 $gameEvent->fill($all);
