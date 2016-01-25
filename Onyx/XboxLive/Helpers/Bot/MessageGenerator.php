41d5ded05514f9939ac95834c1cf28db80a74480
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-01-25 06:18:59 -0600

    add mnt tz

--- Onyx/XboxLive/Helpers/Bot/MessageGenerator.php
+++ Onyx/XboxLive/Helpers/Bot/MessageGenerator.php
@@ -17,7 +17,12 @@ class MessageGenerator {
     /**
      * @var string
      */
-    public static $pacific = 'piece/selkies';
+    public static $pacific = 'selkies';
+
+    /**
+     * @var string
+     */
+    public static $mountain = 'piece';
 
     /**
      * @return string
@@ -27,10 +32,12 @@ class MessageGenerator {
         $cst = new Carbon('now', 'America/Chicago');
         $est = new Carbon('now', 'America/New_York');
         $pac = new Carbon('now', 'America/Los_Angeles');
+        $mnt = new Carbon('now', 'America/Phoenix');
 
         $msg = '<strong>Timezones of Pandas</strong><br /><br />';
 
         $msg .= '<strong>' . self::$pacific . '</strong> - ' . $pac->format('g:ia') . '<br />';
+        $msg .= '<strong>' . self::$mountain . '</strong> - ' . $mnt->format('g:ia') . '<br />';
         $msg .= '<strong>' . self::$central . '</strong> - ' . $cst->format('g:ia') . '<br />';
         $msg .= '<strong>' . self::$eastern . '</strong> - ' . $est->format('g:ia') . '<br />';
 
