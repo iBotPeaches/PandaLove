a7422081bb273d82a3d151d67dd66d1d020192a9
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-02-27 09:03:28 -0500

    Tabs Baby

--- app/Http/Controllers/Halo5/StatsController.php
+++ app/Http/Controllers/Halo5/StatsController.php
@@ -1,6 +1,7 @@
 <?php namespace PandaLove\Http\Controllers\Halo5;
 
 use Illuminate\Http\Request;
+use Illuminate\View\Factory;
 use Onyx\Halo5\Objects\HistoricalStat;
 use PandaLove\Http\Controllers\Controller;
 use PandaLove\Http\Requests;
@@ -18,23 +19,37 @@ class StatsController extends Controller {
      */
     private $db;
 
-    public function __construct(Request $request, DB $db)
+    /**
+     * @var Factory
+     */
+    private $view;
+
+    public function __construct(Request $request, DB $db, Factory $view)
     {
         parent::__construct();
         $this->request = $request;
         $this->db = $db;
+        $this->view = $view;
     }
 
     public function getIndex()
     {
         $graphs = [
-            '#arena_chart' => [
-                'url' =>  action('Halo5\StatsController@getArenaStats'),
-                'type' => 'KD Ratio'
+            [
+                'title' => 'Arena KD',
+                'slug' => 'arena_kd',
             ],
-            '#warzone_chart' => [
-                'url' => action('Halo5\StatsController@getWarzoneStats'),
-                'type' => 'KD Ratio'
+            [
+                'title' => 'Arena KDA',
+                'slug' => 'arena_kda'
+            ],
+            [
+                'title' => 'Warzone KD',
+                'slug' => 'warzone_kd'
+            ],
+            [
+                'title' => 'Warzone KDA',
+                'slug' => 'warzone_kda'
             ]
         ];
 
@@ -45,13 +60,68 @@ class StatsController extends Controller {
         ]);
     }
 
-    public function getArenaStats()
+    public function getIndividualGraph($type)
+    {
+        $allowed = ["arena_kd", "arena_kda", "warzone_kd", "warzone_kda"];
+
+        if (in_array($type, $allowed))
+        {
+            switch ($type)
+            {
+                case "arena_kd":
+                    return $this->view->make('includes.halo5.stats._graph', [
+                        'data' => [
+                            'type' => $type,
+                            'url' => action('Halo5\StatsController@getArenaKDStats'),
+                            'selector' => '#' . $type,
+                            'y_axis' => 'KD Ratio'
+                        ]
+                    ]);
+
+                case "arena_kda":
+                    return $this->view->make('includes.halo5.stats._graph', [
+                        'data' => [
+                            'type' => $type,
+                            'url' => action('Halo5\StatsController@getArenaKDAStats'),
+                            'selector' => '#' . $type,
+                            'y_axis' => 'KDA Ratio'
+                        ]
+                    ]);
+
+                case "warzone_kd":
+                    return $this->view->make('includes.halo5.stats._graph', [
+                        'data' => [
+                            'type' => $type,
+                            'url' => action('Halo5\StatsController@getWarzoneKDStats'),
+                            'selector' => '#' . $type,
+                            'y_axis' => 'KD Ratio'
+                        ]
+                    ]);
+
+                case "warzone_kda":
+                    return $this->view->make('includes.halo5.stats._graph', [
+                        'data' => [
+                            'type' => $type,
+                            'url' => action('Halo5\StatsController@getWarzoneKDAStats'),
+                            'selector' => '#' . $type,
+                            'y_axis' => 'KDA Ratio'
+                        ]
+                    ]);
+            }
+        }
+        else
+        {
+            return $this->view->make('includes.halo5.stats._overview');
+        }
+    }
+
+    public function getArenaKDStats()
     {
         /** @var $stats HistoricalStat[] */
         $stats = $this->db
             ->table('halo5_stats_history as H')
             ->join('accounts as A', 'A.id', '=', 'H.account_id')
-            ->select('H.account_id', 'H.arena_kd', 'H.arena_kda', 'H.arena_total_games', 'H.date', 'A.gamertag')
+            ->select('H.account_id', 'H.arena_kd', 'H.arena_total_games', 'H.date', 'A.gamertag')
             ->orderBy('date', 'ASC')
             ->get();
 
@@ -63,8 +133,7 @@ class StatsController extends Controller {
             {
                 $data['c3']['x'][] = $stat->date;
             }
-            $data['c3'][$stat->gamertag . ' KD'][] = $stat->arena_kd;
-            $data['c3'][$stat->gamertag . ' KDA'][] = $stat->arena_kda;
+            $data['c3'][$stat->gamertag][] = $stat->arena_kd;
             $data['totalGames'][$stat->gamertag][] = $stat->arena_total_games;
         }
         arsort($data['c3']);
@@ -72,19 +141,43 @@ class StatsController extends Controller {
         return json_encode($data, true, JSON_NUMERIC_CHECK);
     }
 
-    public function getWarzoneStats()
+    public function getArenaKDAStats()
     {
         /** @var $stats HistoricalStat[] */
         $stats = $this->db
             ->table('halo5_stats_history as H')
             ->join('accounts as A', 'A.id', '=', 'H.account_id')
-            ->select('H.account_id', 'H.warzone_kd', 'H.warzone_kda', 'H.warzone_total_games', 'H.date', 'A.gamertag')
+            ->select('H.account_id', 'H.arena_kda', 'H.arena_total_games', 'H.date', 'A.gamertag')
+            ->orderBy('date', 'ASC')
+            ->get();
+
+        $data = ['c3' => ['x' => [$stats[0]->date]]];
+
+        foreach ($stats as $stat)
+        {
+            if (! in_array($stat->date, $data['c3']['x']))
+            {
+                $data['c3']['x'][] = $stat->date;
+            }
+            $data['c3'][$stat->gamertag][] = $stat->arena_kda;
+            $data['totalGames'][$stat->gamertag][] = $stat->arena_total_games;
+        }
+        arsort($data['c3']);
+
+        return json_encode($data, true, JSON_NUMERIC_CHECK);
+    }
+
+    public function getWarzoneKDStats()
+    {
+        /** @var $stats HistoricalStat[] */
+        $stats = $this->db
+            ->table('halo5_stats_history as H')
+            ->join('accounts as A', 'A.id', '=', 'H.account_id')
+            ->select('H.account_id', 'H.warzone_kd', 'H.warzone_total_games', 'H.date', 'A.gamertag')
             ->orderBy('date', 'DESC')
             ->get();
 
-        $data = ['c3' => [
-            'x' => [$stats[0]->date]
-        ]];
+        $data = ['c3' => ['x' => [$stats[0]->date]]];
 
         foreach ($stats as $stat)
         {
@@ -93,11 +186,36 @@ class StatsController extends Controller {
                 $data['c3']['x'][] = $stat->date;
             }
 
-            $data['c3'][$stat->gamertag . ' KD'][] = $stat->warzone_kd;
-            $data['c3'][$stat->gamertag . ' KDA'][] = $stat->warzone_kda;
+            $data['c3'][$stat->gamertag][] = $stat->warzone_kd;
             $data['totalGames'][$stat->gamertag][] = $stat->warzone_total_games;
         }
+        arsort($data['c3']);
 
+        return json_encode($data, true, JSON_NUMERIC_CHECK);
+    }
+
+    public function getWarzoneKDAStats()
+    {
+        /** @var $stats HistoricalStat[] */
+        $stats = $this->db
+            ->table('halo5_stats_history as H')
+            ->join('accounts as A', 'A.id', '=', 'H.account_id')
+            ->select('H.account_id', 'H.warzone_kda', 'H.warzone_total_games', 'H.date', 'A.gamertag')
+            ->orderBy('date', 'DESC')
+            ->get();
+
+        $data = ['c3' => ['x' => [$stats[0]->date]]];
+
+        foreach ($stats as $stat)
+        {
+            if (! in_array($stat->date, $data['c3']['x']))
+            {
+                $data['c3']['x'][] = $stat->date;
+            }
+
+            $data['c3'][$stat->gamertag][] = $stat->warzone_kda;
+            $data['totalGames'][$stat->gamertag][] = $stat->warzone_total_games;
+        }
         arsort($data['c3']);
 
         return json_encode($data, true, JSON_NUMERIC_CHECK);
--- resources/views/halo5/historic_stats.blade.php
+++ resources/views/halo5/historic_stats.blade.php
@@ -8,11 +8,26 @@
                     <header>
                         <h1>Welcome to our <strong>Graph</strong></h1>
                     </header>
-                    <h4>Arena Stats</h4>
-                    <div id="arena_chart" class="graph"></div>
-                    <br />
-                    <h4>Warzone Stats</h4>
-                    <div id="warzone_chart" class="graph"></div>
+                    <div class="ui secondary menu" id="graph-tabs">
+                        <div class="item active" data-tab="overview">Overview</div>
+                        @foreach($graphs as $graph)
+                            <div class="item" data-tab="{{ $graph['slug'] }}">{{ $graph['title'] }}</div>
+                        @endforeach
+                    </div>
+                    <div class="ui tab active" data-tab="overview">
+                        <div class="ui info message">
+                            The graphs here are updated nightly at 22:00 UTC (4pm CST). If your character has not played
+                            Halo in 10 days, then it will be marked as inactive and thus not updated.
+                            <br /><br />
+                            Please ensure that you issue a <strong>/bot h5</strong> command in chat when you start playing again.
+                            If your stats change from the result of that command, you will no longer be marked as inactive.
+                        </div>
+                    </div>
+                    @foreach($graphs as $graph)
+                        <div class="ui tab" data-tab="{{ $graph['slug'] }}" data-path="{{ $graph['slug'] }}">
+
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </article>
@@ -32,5 +47,14 @@
 @section('inline-js')
     <script src="//d3js.org/d3.v3.min.js"></script>
     <script src="{{ asset("js/c3.min.js") }}"></script>
-    @include('includes.halo5.stats.graphs', ['data' => $graphs])
+    <script type="text/javascript">
+        $(function() {
+           $('.menu .item').tab({
+               auto: true,
+               cache: false,
+               evaluteScripts: true,
+               path: '/h5/stats/individual-graph/'
+           });
+        });
+    </script>
 @append
