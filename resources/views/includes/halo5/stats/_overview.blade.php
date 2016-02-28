4fe9b82a6ff8c9dc6c65f6eeffd266bf04c93688
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-02-28 09:53:34 -0500

    remove wrong update text

--- resources/views/includes/halo5/stats/_overview.blade.php
+++ resources/views/includes/halo5/stats/_overview.blade.php
@@ -1,6 +1,5 @@
 <div class="ui info message">
-    The graphs here are updated nightly at 22:00 UTC (4pm CST). If your character has not played
-    Halo in 10 days, then it will be marked as inactive and thus not updated.
+    If your character has not played Halo in 10 days, then it will be marked as inactive and thus not updated.
     <br /><br />
     Please ensure that you issue a <strong>/bot h5</strong> command in chat when you start playing again.
     If your stats change from the result of that command, you will no longer be marked as inactive.
