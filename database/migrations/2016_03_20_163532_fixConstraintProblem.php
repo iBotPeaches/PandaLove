6629f8bd4a91cd9f18a629f377b96c59d9d9cd38
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-03-20 17:43:43 -0400

    some profiles reference this season

---
+++ database/migrations/2016_03_20_163532_fixConstraintProblem.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixConstraintProblem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $season = new \Onyx\Halo5\Objects\Season();
+        $season->contentId = 'Nonseasonal';
+        $season->name = 'No Season';
+        $season->start_date = new \Carbon\Carbon('2014-03-01');
+        $season->end_date = new \Carbon\Carbon('2014-03-02');
+        $season->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \Onyx\Halo5\Objects\Season::where('contentId', 'Nonseasonal')->delete();
+    }
+}
