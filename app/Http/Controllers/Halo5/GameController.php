fbcedd4dd030853e72ef8d63b32a8bb2b75edcd3
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-04-06 09:27:22 -0400

    initial layout of views for match-events and game

--- app/Http/Controllers/Halo5/GameController.php
+++ app/Http/Controllers/Halo5/GameController.php
@@ -37,6 +37,26 @@ class GameController extends Controller {
                 ->where('uuid', $matchId)->firstOrFail();
 
             return $match;
+            return view('halo5.games.game', [
+                'match' => $match
+            ]);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            \App::abort(404);
+        }
+    }
+
+    public function getMatchEvents($matchId)
+    {
+        try
+        {
+            $match = Match::with('events.assists', 'events.killer_weapon', 'events.victim_weapon', 'events.victim.h5', 'events.killer.h5')
+                ->where('uuid', $matchId)->firstOrFail();
+
+            return view('halo5.games.events', [
+                'match' => $match
+            ]);
         }
         catch (ModelNotFoundException $e)
         {
---
+++ resources/views/halo5/games/game.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('content')
+    <div class="wrapper style1">
+        <article class="container no-image" id="top">
+            <div class="row">
+                <div class="12u">
+                    <h1 class="ui header">
+                        [FUTURE] POST GAME CARNAGE REPORT
+                    </h1>
+                </div>
+            </div>
+        </article>
+    </div>
+@endsection
+
+@section('inline-css')
+@append
