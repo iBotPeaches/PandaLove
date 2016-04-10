45a3f3f6f0b5997154b1d202f4bf865b59581446
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-04-10 14:56:24 -0400

    fixes #47

---
+++ database/migrations/2016_04_10_134800_addSomeIndexes.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSomeIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('destiny_data', function (Blueprint $table)
+        {
+            $table->index('created_at');
+        });
+
+        Schema::table('accounts', function (Blueprint $table)
+        {
+            $table->index('seo');
+            $table->index('accountType');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('destiny_data', function (Blueprint $table)
+        {
+            $table->dropIndex('destiny_data_created_at_index');
+        });
+
+        Schema::table('accounts', function (Blueprint $table)
+        {
+            $table->dropIndex('accounts_seo_index');
+            $table->dropIndex('accounts_accountType_index');
+        });
+    }
+}
