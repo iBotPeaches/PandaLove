82975857be48044f6570714dd40495824a2e1b6a
Connor Tumbleson <connor.tumbleson@gmail.com>
2016-04-23 07:46:35 -0400

    update unit-tests to test panda api for leaf

--- build_script.sh
+++ build_script.sh
@@ -6,5 +6,6 @@ npm install gulp;
 mysql --user=connor_testing --password=gmsZbtvEhEM9 -BNe "show tables" connor_testing | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=connor_testing --password=gmsZbtvEhEM9 connor_testing;
 ./artisan migrate --env=testing;
 ./artisan db:seed --env=testing;
+./artisan halo5:batch-metadata
 gulp
 ./vendor/bin/phpunit;
