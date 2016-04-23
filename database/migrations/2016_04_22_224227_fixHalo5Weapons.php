cef810e462fb210f2487bb3b17dd4715337ba954
Brandon Lile <brandon.lile@gmail.com>
2016-04-22 22:59:42 -0500

    Removal of id from halo5_weapons. Also, issue69. lol

---
+++ database/migrations/2016_04_22_224227_fixHalo5Weapons.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixHalo5Weapons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('halo5_weapons', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('halo5_weapons', function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+}
