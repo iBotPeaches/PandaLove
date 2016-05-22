e59faa3f9538afc6b34437c28cdf9ef34727aa56
Connor Tumbleson <me@connortumbleson.com>
2016-05-22 16:57:16 -0400

    fixes #80
     - changes size of impulse name to 64 chars (previously 32)

---
+++ database/migrations/2016_05_22_155507_growImpulseNameField.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GrowImpulseNameField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('halo5_impulses', function (Blueprint $table)
+        {
+            $table->string('name', 64)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('halo5_impulses', function (Blueprint $table)
+        {
+            $table->string('name', 32)->change();
+        });
+    }
+}
