8dbe834ca4f77dce7fa4e1d567e6ffd8b877f29a
Connor Tumbleson <me@connortumbleson.com>
2016-08-21 11:48:57 -0400

    make name nullable on halo5_maps

---
+++ database/migrations/2016_08_21_104418_makeNameNullable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeNameNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `halo5_maps` CHANGE `name` `name` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
