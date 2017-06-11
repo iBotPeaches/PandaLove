8f26e16afa5623cf45d04c259b361cd473edd511
Connor Tumbleson <me@connortumbleson.com>
2017-06-11 17:39:55 -0400

    3 stats

--- Onyx/Overwatch/Helpers/Bot/MessageGenerator.php
+++ Onyx/Overwatch/Helpers/Bot/MessageGenerator.php
@@ -8,6 +8,11 @@ use Onyx\Overwatch\Objects\Stats;
 class MessageGenerator
 {
     /**
+     * @var array
+     */
+    private static $ignoredAttributes = ['avatar', 'rank_image', 'account_id', 'season', 'id'];
+
+    /**
      * @param Account $account
      * @param Stats $old
      * @param Stats $new
@@ -17,21 +22,26 @@ class MessageGenerator
     {
         $msg = '';
 
+        $stats = [];
         $random_key = array_rand($old->getAttributes(), 1);
 
-        while ($random_key == 'avatar' || $random_key == 'rank_image' || $random_key == 'account_id' || $random_key == 'season' || $random_key == 'id') {
+        while (! in_array($random_key, self::$ignoredAttributes) && count($stats) < 3) {
             $random_key = array_rand($old->getAttributes(), 1);
+
+            $difference = $new->$random_key - $old->$random_key;
+            $stats[$random_key] = $difference;
         }
 
-        $difference = $new->$random_key - $old->$random_key;
 
         $msg .= '<strong>'.$account->gamertag.'</strong> stats have been updated!<br />';
         $msg .= 'Level: <strong>' . $new->totalLevel() . "</strong><br />";
         $msg .= 'SR (current/high): <strong>' . $new->comprank . ' / ' . $new->max_comprank . '</strong><br />';
 
-        $msg .= '<br />Random Stat:<br />';
-        $msg .= ucfirst(str_replace('_', ' ', $random_key)) . ': ';
-        $msg .= $new->$random_key . ' (' . sprintf("%+d", $difference) . ')';
+        $msg .= '<br />Random Stats:<br />';
+        foreach ($stats as $key => $difference) {
+            $msg .= ucfirst(str_replace('_', ' ', $key)) . ': ';
+            $msg .= $new->$key . ' (' . sprintf("%+d", $difference) . ') <br />';
+        }
 
         return $msg;
     }
