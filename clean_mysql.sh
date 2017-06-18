e56a4587212d0d133b21020d657976742ee3f9ce
Connor Tumbleson <me@connortumbleson.com>
2017-06-18 13:22:53 -0400

    clean db script

--- clean_mysql.sh
+++ clean_mysql.sh
@@ -1,2 +1,24 @@
-#!/usr/bin/env bash
-mysql --user=`echo $DB_USERNAME` --password=`echo $DB_PASSWORD` -BNe "show tables" echo `echo $DB_DATABASE` | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql --user=`echo $DB_USERNAME` --password=`echo $DB_PASSWORD` `echo $DB_DATABASE`;
+#!/bin/bash
+MUSER="$1"
+MPASS="$2"
+MDB="$3"
+
+# Detect paths
+MYSQL=$(which mysql)
+AWK=$(which awk)
+GREP=$(which grep)
+
+if [ $# -ne 3 ]
+then
+	echo "Usage: $0 {MySQL-User-Name} {MySQL-User-Password} {MySQL-Database-Name}"
+	echo "Drops all tables from a MySQL"
+	exit 1
+fi
+
+TABLES=$($MYSQL -u $MUSER -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
+
+for t in $TABLES
+do
+	echo "Deleting $t table from $MDB database..."
+	$MYSQL -u $MUSER -p$MPASS $MDB -e "drop table $t"
+done
