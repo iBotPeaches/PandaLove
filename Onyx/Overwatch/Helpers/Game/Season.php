dc07510dd318b1fda4e918dd7998fa029d16f12d
Connor Tumbleson <me@connortumbleson.com>
2017-08-29 20:20:32 -0400

    update for Season 6

--- Onyx/Overwatch/Helpers/Game/Season.php
+++ Onyx/Overwatch/Helpers/Game/Season.php
@@ -32,7 +32,7 @@ class Season
         ],
         5 => [
             'start'  => 'June 1, 2017',
-            'end'    => 'August 30, 2017',
+            'end'    => 'August 31, 2017',
             'season' => 5,
         ],
         6 => [
