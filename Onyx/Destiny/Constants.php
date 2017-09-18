26fe68ec46f61585e0d6ea74ea2e7d75e89112f2
Connor Tumbleson <connor.tumbleson@gmail.com>
2017-09-18 09:00:11 -0400

    D1: Fix URLs to work

--- Onyx/Destiny/Constants.php
+++ Onyx/Destiny/Constants.php
@@ -9,7 +9,7 @@ class Constants
      *
      * @var string
      */
-    public static $searchDestinyPlayer = 'https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/%1$s/%2$s';
+    public static $searchDestinyPlayer = 'https://www.bungie.net/d1/Platform/Destiny/SearchDestinyPlayer/%1$s/%2$s';
 
     /**
      * Obtain basic information (weekly, characters, etc) about a membershipId.
@@ -17,35 +17,35 @@ class Constants
      * @var string
      * @url https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2f
      */
-    public static $platformDestiny = 'https://www.bungie.net/Platform/Destiny/%1$d/Account/%2$s/Summary';
+    public static $platformDestiny = 'https://www.bungie.net/d1/Platform/Destiny/%1$d/Account/%2$s/Summary';
 
     /**
      * Obtain profile information (motto, clans, groups) about a membershipId / membershipType.
      *
      * @var string
      */
-    public static $getBungieAccount = 'https://www.bungie.net/Platform/User/GetBungieNetUserById/%1$s/%2$d';
+    public static $getBungieAccount = 'https://www.bungie.net/d1/Platform/User/GetBungieNetUserById/%1$s/%2$d';
 
     /**
      * Obtain Post Game Carnage Report for a game (instanceId).
      *
      * @var string
      */
-    public static $postGameCarnageReport = 'https://www.bungie.net/platform/Destiny/Stats/PostGameCarnageReport/%1$s';
+    public static $postGameCarnageReport = 'https://www.bungie.net/d1/platform/Destiny/Stats/PostGameCarnageReport/%1$s';
 
     /**
      * Obtain Item hashes / emblems.
      *
      * @var string
      */
-    public static $explorerItems = 'https://www.bungie.net/Platform/Destiny/Manifest/InventoryItem/%1$s';
+    public static $explorerItems = 'https://www.bungie.net/d1/Platform/Destiny/Manifest/InventoryItem/%1$s';
 
     /**
      * Obtain XUR data when present.
      *
      * @var string
      */
-    public static $xurData = 'https://www.bungie.net/Platform/Destiny/Advisors/Xur';
+    public static $xurData = 'https://www.bungie.net/d1/Platform/Destiny/Advisors/Xur';
 
     /**
      * URL to items on guardian.gg.
